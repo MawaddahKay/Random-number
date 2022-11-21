1679c1189adebbc6881a4fbd1767d901e2256255
MawaddahKay <101088983+MawaddahKay@users.noreply.github.com>
2022-11-21 09:54:57 +0700

    .

--- number.py
+++ number.py
@@ -3,14 +3,14 @@ Program: random.com
 Author: Mawaddah
 Last date modified:21/11/22
 """
-# guess the number game in Python by CodeSpeedy.com
+# guess the number game in Python
 import random
-random_number = random.randint(1,100)
+random_number = random.randint(1,10)
 win = False
 Turns =0
 while win==False:
-    Your_guess = input("Enter a number between 1 and 100")
-    Turns +=1
+    Your_guess = input("Enter a number between 1 and 10")
+    Turns -=1
     if random_number==int(Your_guess):
         print("You won!")
         print("Number of turns you have used: ",Turns)
