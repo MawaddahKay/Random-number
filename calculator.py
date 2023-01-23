e4f4689e4b1c34d8908f83f0f81e9d83e2b03747
MawaddahKay <101088983+MawaddahKay@users.noreply.github.com>
2023-01-23 13:15:58 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,49 +1,54 @@
+Author: Mawaddah Kay
 #calculate the two numbers
-# user enter 2 number
+
+# This function addition of the two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+# user input the number
 while True:
-        try:
-            try:
-                number1 = float(input("Input  the number:"))     #we need to put 'int' before the code if u dont put the result will be string not numeric ex '5'+'5'=55 not 10
-            except :
-                print("Your first input is not a number")
-            try:   
-                number2 = float(input('Input the number:'))
-            except:
-                print("Your second input is not a number")
-            x = input("select the operator (+,-,*,/): ")     
-        except TypeError:
-            print("You pass incorrect argument value" )
-        except ValueError:
-            print("Pleas enter the correct value")
-        except ZeroDivisionError:
-            print('Cannot devide by zero')   
-       
+    try:
+        number1 = float(input("Input  the number:"))     
+        number2 = float(input('Input the second number:'))
+        x = input("select the operator (+,-,*,/): ")  
+    except ValueError:
+        print("Please enter the correct value")
+        continue
 
-        def add(x, y):
-            return x + y
-
-        # This function subtracts two numbers
-        def subtract(x, y):
-            return x - y
-
-        # This function multiplies two numbers
-        def multiply(x, y):
-            return x * y
-
-        # This function divides two numbers
-        def divide(x, y):
-            return x / y
-
-
-
-#calculate the total of 2 number
+    try:
+    #calculate the total of 2 number
         if x == '+':
             print(number1,'+',number2,'=',add(number1,number2))
         elif x == '-':
             print(number1,'-',number2,'=',subtract(number1,number2))
         elif x == '*':
             print(number1,'*',number2,'=',multiply(number1,number2))
-        else:
+        elif x =='/':
             print(number1,'/',number2,'=',divide(number1,number2))
+        
+    except ZeroDivisionError:
+        print("Cannot devide by zero")
+    except TypeErro:
+        print("You past")
+
+# check if user wants another calculation
+    # break the while loop if answer is no
+    next_calculation = input("Do you want to calculate again? (yes/no): ")
+    if next_calculation == "no":
+        break
+    else:
+        print("please choose the correct operator(+, -, *,/)")
     
-            continue 
+           
